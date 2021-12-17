70f6eb59e7760c4e374abd7a863cfc273df89470
Apleon <shara49750@gmail.com>
2021-12-17 21:50:47 +0300

    add gitignore and add function in main.rb

---
+++ andrew/main.rb
@@ -0,0 +1,5 @@
+def equation_1(veriable_s, veriable_t, veriable_x)
+    veriable_t = Math.tan(veriable_x.pow(2)) + Math.sqrt(veriable_x) 
+    /variable_t * Math.log(veriable_x + veriable_s)
+    puts(veriable_t)
+
