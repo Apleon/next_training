5c1807909004fe4563dcbf721679278b01ff6fd7
Apleon <shara49750@gmail.com>
2021-12-18 03:14:40 +0300

    try to use 2 (not 4) spaces for indentation.

--- andrew/main.rb
+++ andrew/main.rb
@@ -1,7 +1,7 @@
 #task1.1
 
 def equation_1(veriable_s, veriable_t, veriable_x)
-    (Math.tan(veriable_x.pow(2)) + Math.sqrt(veriable_x)) / 
+  (Math.tan(veriable_x.pow(2)) + Math.sqrt(veriable_x)) / 
     (veriable_t * Math.log2(veriable_x + veriable_s))
 end
 
@@ -11,54 +11,54 @@ def equation_2(veriable_s, veriable_t, veriable_x)
 end
 
 def equation_3(veriable_s, veriable_t, veriable_x)
-    (Math.exp(0.5 * veriable_x)) / 
+  (Math.exp(0.5 * veriable_x)) / 
     (Math.sqrt(veriable_x + veriable_s * 
-        Math.log10(veriable_x.pow(veriable_t))))
+      Math.log10(veriable_x.pow(veriable_t))))
 end
 
 def equation_4(veriable_s, veriable_t, veriable_x)
-    (Math.atan(Math.sqrt(veriable_x.pow(veriable_t)))) / 
+  (Math.atan(Math.sqrt(veriable_x.pow(veriable_t)))) / 
     (veriable_x.pow(2) + veriable_s * Math.sin(Math.log10(veriable_x)))
 end
 
 def equation_5(veriable_s, veriable_t, veriable_x)
-    (veriable_x.pow(veriable_t)) / 
+  (veriable_x.pow(veriable_t)) / 
     (Math.sqrt(veriable_x.pow(3) + 1) + Math.log10(veriable_s))
 end
 
 def equation_6(veriable_s, veriable_t, veriable_x)
-    (Math.sin(veriable_x.pow(veriable_t).pow(2))) / 
+  (Math.sin(veriable_x.pow(veriable_t).pow(2))) / 
     (Math.sqrt(1 + veriable_x.pow(veriable_s)))
 end
 
 def equation_7(veriable_s, veriable_t, veriable_x)
-    (1 + veriable_x.pow(veriable_t) + Math.log10(veriable_x.pow(3))) / 
+  (1 + veriable_x.pow(veriable_t) + Math.log10(veriable_x.pow(3))) / 
     (Math.sqrt(veriable_x.pow(veriable_s) + 1))
 end
 
 def equation_8(veriable_s, veriable_t, veriable_x)
-    (Math.cos(veriable_x.pow(2).pow(3))) / 
+  (Math.cos(veriable_x.pow(2).pow(3))) / 
     (veriable_s * Math.sqrt(veriable_x.pow(veriable_t)))
 end
 
 def equation_9(veriable_t, veriable_x)
-    veriable_x.pow(2) / 
+  veriable_x.pow(2) / 
     (Math.log10(veriable_x.pow(veriable_t))) + 
-     (Math.cos(veriable_x.pow(veriable_t).pow(2)))
+      (Math.cos(veriable_x.pow(veriable_t).pow(2)))
 end
 
 def equation_10(veriable_s, veriable_t, veriable_x)
-    (Math.sqrt(veriable_t)) * (3 * veriable_x.pow(veriable_t)) / 
+  (Math.sqrt(veriable_t)) * (3 * veriable_x.pow(veriable_t)) / 
     (Math.sqrt(1 + veriable_x.pow(veriable_s)))
 end
 
 def equation_11(veriable_s, veriable_t, veriable_x)
-    Math.sqrt(veriable_t + veriable_x.pow(3)) ** veriable_s / 
+  Math.sqrt(veriable_t + veriable_x.pow(3)) ** veriable_s / 
     Math.log10(veriable_x.pow(veriable_t))
 end
 
 def equation_12(veriable_s, veriable_t, veriable_x)
-    (1 + veriable_x.pow(2) ** veriable_t) / 
+  (1 + veriable_x.pow(2) ** veriable_t) / 
     Math.exp(Math.sin(veriable_x) + veriable_s)
 end
 
